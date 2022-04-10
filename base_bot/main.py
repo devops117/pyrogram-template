500f186cd67bbadce07f23b346c8850182f8befa
devops117 <55235206+devops117@users.noreply.github.com>
2022-04-10 21:16:30 +0530

    Update config struct and logic for creating clients
    Signed-off-by: devops117 <55235206+devops117@users.noreply.github.com>

--- base_bot/main.py
+++ base_bot/main.py
@@ -24,9 +24,9 @@
 import asyncio
 import logging
 from operator import itemgetter
-import os # os.path, os.environ
+import os # os.environ
 from pathlib import Path
-from typing import TypedDict
+from typing import NamedTuple, TypedDict
 
 from ruamel.yaml import YAML
 import pyrogram # pyrogram.Client
@@ -35,67 +35,68 @@ logging.basicConfig(level=logging.INFO)
 
 pyrogram_sessions_dir=Path("sessions")
 
-config_file=Path("config.yaml")
 yaml=YAML(typ="safe")
-data=yaml.load(config_file) # look for ./config.yaml
-
-CLIENTS=None
-
-pyrogram_client_data = TypedDict(
-    "pyrogram_client_data",
-    {
-        "session_name": str,
-
-        "TELEGRAM_API_ID": int,
-        "TELEGRAM_API_HASH": str,
-        "TELEGRAM_BOT_TOKEN": str | None,
-
-        "wheel_userids": list[int],
-
-        "plugins": TypedDict(
-            "plugins_data",
-            {
-                "root": str,
-                "include": list[str] | None,
-                "exclude": list[str] | None,
-            },
-        ),
-    },
-)
+CONFIG_DATA = yaml.load(Path("config.yaml")) # look for ./config.yaml
 
 
-def client_init(clients_data: list[pyrogram_client_data] = data.get("clients")) -> list[pyrogram.Client] | None:
+class ClientStore(NamedTuple):
+    session_name: str
+    client: pyrogram.Client
+
+
+class PluginsData(TypedDict):
+    root: str
+    include: list[str] | None
+    exclude: list[str] | None
+
+
+class ClientConfigData(TypedDict):
+    TELEGRAM_API_ID: int
+    TELEGRAM_API_HASH: str
+    TELEGRAM_BOT_TOKEN: str | None
+    wheel_userids: list[int]
+    plugins: PluginsData
+
+
+def client_create(session_name: str, client_data: ClientConfigData) -> ClientStore:
     global pyrogram_sessions_dir # sessions directory
-    
-    for client_data in clients_data:
 
-        api_id, api_hash = itemgetter(
-                *(itemgetter("TELEGRAM_API_ID", "TELEGRAM_API_HASH")(client_data))
-            )(os.environ)
-        if bot_token := client_data.get("TELEGRAM_BOT_TOKEN"):
-            bot_token = os.environ.get(bot_token)
+    api_id, api_hash = itemgetter(
+            *(itemgetter("TELEGRAM_API_ID", "TELEGRAM_API_HASH")(client_data))
+        )(os.environ)
+    if bot_token := client_data.get("TELEGRAM_BOT_TOKEN"):
+        bot_token = os.environ.get(bot_token)
 
-        client = pyrogram.Client(
-            client_data["session_name"],
-            api_id,
-            api_hash,
-            bot_token=bot_token,
-            plugins=client_data["plugins"],
-            workdir=pyrogram_sessions_dir,
-        )
+    client = pyrogram.Client(
+        session_name,
+        api_id,
+        api_hash,
+        bot_token=bot_token,
+        plugins=client_data["plugins"],
+        workdir=pyrogram_sessions_dir,
+    )
+    return ClientStore(session_name, client)
+
+
+def client_init(clients_data: list[dict[str, ClientConfigData]]) -> ClientStore:
+    """
+    You can use dict.pop() on client_data;
+    in case a particular session should be dealt with in a special manner
+    """
+
+    for session_name, client_data in clients_data.items():
+        client_created = client_create(session_name, client_data)
 
         # A way to feed values, can be accessed using update handler's callback(client)
-        client.wheel_userids = client_data["wheel_userids"]
-        yield client
+        client_created.client.wheel_userids = client_data["wheel_userids"]
+        yield client_created
 
 
 async def async_main() -> None:
-    global CLIENTS
+    global CONFIG_DATA
 
-    CLIENTS = [
+    for session_name, client in client_init(CONFIG_DATA.get("clients")):
         await client.start()
-        for client in client_init()
-    ]
     await pyrogram.idle()
 
 
